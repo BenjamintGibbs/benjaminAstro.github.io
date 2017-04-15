cb738ec9d295b00c21eb421e3b2d21efd83ab9ee
BenjamintGibbs <benjamintgibbs710@gmail.com>
2017-04-15 20:39:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// CODEPEN BY Leon Grasmeijer
-
-//http://codepen.io/LeonGr/
 
 
 var canvas = document.getElementById("canvas"),
@@ -13,56 +10,61 @@ var stars = [], // Array that contains the stars
     FPS = 60, // Frames per second
     x = canvas.width; // Number of stars
 
-// Push stars to array
-
-for (var i = 0; i < x; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random(),
-    vx: Math.floor(Math.random() * 10) - 5,
-    vy: Math.floor(Math.random() * 10) - 5
-  });
-}
-
-// Draw the scene
+    for (i = 0; i < x; i++)
+    {
+      stars[i] = new Star();
+    }
 
 function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
 
-  ctx.globalCompositeOperation = "lighter";
+      ctx.clearRect(0,0,canvas.width,canvas.height);
 
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
+      ctx.globalCompositeOperation = "lighter";
 
+      for (var i = 0, x = stars.length; i < x; i++) {
+
+        stars[i].move();
+        stars[i].display();
+        stars[i].update();
+
+      }
+    }
+
+function Star() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height;
+  this.radius = Math.random();
+  this.vx = Math.floor(Math.random() * 10) - 5;
+  this.vy = Math.floor(Math.random() * 10) - 5;
+
+  this.display = function() {
     ctx.fillStyle = "#fff";
     ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y,this.radius, 0, 2 * Math.PI);
     ctx.fill();
-  }
-}
+  };
+  this.move = function() {
+    this.x += this.vx / FPS;
+    this.y += this.vy / FPS;
+  };
+  this.update = function() {
+    if (this.x < 0 || this.x > canvas.width) this.x = -this.x;
+    if (this.y < 0 || this.y > canvas.height) this.y = -this.y;
+  };
+ }
 
-// Update star locations
+ function tick() {
+   draw();
+   requestAnimationFrame(tick);
+ }
 
-function update() {
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
-
-    if (s.x < 0 || s.x > canvas.width) s.x = -s.x;
-    if (s.y < 0 || s.y > canvas.height) s.y = -s.y;
-  }
-}
-
-// Update and draw
-
-function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
-}
-
-tick();
-
+ tick();
+ $(document).ready(function() {
+ $('.thumbnail').click(function(){
+       $('.modal-body').empty();
+   	var title = $(this).parent('a').attr("title");
+   	$('.modal-title').html(title);
+   	$($(this).parents('div').html()).appendTo('.modal-body');
+   	$('#myModal').modal({show:true});
+ });
+ });
